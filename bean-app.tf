d8dbd6043bb640f7217ee8d7459fd05ec37c55c5
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-10-18 16:16:41 +0300

    Update bean-app.tf

--- bean-app.tf
+++ bean-app.tf
@@ -1,3 +1,7 @@
-resource "aws_elastic_beanstalk_application" "teachua-app-best" {
-  name = "teachua-app"
+data "aws_elastic_beanstalk_application" "existing_teachua_app" {
+  name = "teachua-app"  
 }
+
+# resource "aws_elastic_beanstalk_application" "teachua-app-best" {
+#  name = "teachua-app"
+#}
