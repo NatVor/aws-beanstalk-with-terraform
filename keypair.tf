d662cd2f5bafdb8f69958ca637d8b771aa8751f1
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-10-18 14:25:17 +0300

    Update keypair.tf

--- keypair.tf
+++ keypair.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "teachua_app_key" {
+#resource "aws_key_pair" "teachua_app_key" {
   # key_name   = "teachua-key-1"
-  key_name   = "web02_key"
-  public_key = file(var.PUB_KEY)
-}
+#  key_name   = "web02_key"
+#  public_key = file(var.PUB_KEY)
+#}
