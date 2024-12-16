1219c366175a1924b71a23016ba7f31da2440130
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-12-16 13:44:43 +0200

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "teachua-beanstalk-app-elb-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   # Allow HTTP (port 80) from any IP (public access to the load balancer)
